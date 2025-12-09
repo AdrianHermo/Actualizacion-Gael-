e3e84d0894e582a61b439646cba0922f41326084
Adrian <ahermoabalde@danielcastelao.org>
2025-12-09 14:03:17 +0100

    Barco U1: Implementación del método de tamaño de la unidad.

---
+++ Barco.py
@@ -0,0 +1,10 @@
+class Barco:
+    """Clase que representa un barco en el juego."""
+
+    def __init__(self, unidades):
+        # unidades: lista de posiciones que ocupa el barco
+        self.unidades = unidades
+
+    def tamano(self):
+        """Devuelve el tamaño del barco (número de unidades)."""
+        return len(self.unidades)
