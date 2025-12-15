654fb6c1f372d7eb836089acd275828aeb941283
gael <gmarcelinodocio@danielcastelao.org>
2025-12-15 19:54:34 +0100

    "Tablero U2: Implementación de la cuadrícula de ataque."

---
+++ Tablero.py
@@ -0,0 +1,17 @@
+class Tablero:
+    """Clase que representa el tablero del juego."""
+
+    def __init__(self, filas, columnas):
+        self.filas = filas
+        self.columnas = columnas
+        self.celdas = [["~" for _ in range(columnas)] for _ in range(filas)]
+
+    def mostrar(self):
+        """Muestra el tablero por pantalla."""
+        for fila in self.celdas:
+            print(" ".join(fila))
+
+    def colocar_barco(self, barco):
+        """Coloca un barco en el tablero."""
+        for (fila, columna) in barco.unidades:
+            self.celdas[fila][columna] = "B"
